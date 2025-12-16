de1ee3b1a7ed43a0bafe9da4c283a2b6eaa29bc7
dotUsman <usmanmusawar1@gmail.com>
2025-12-16 23:42:36 +0500

    Add files via upload

---
+++ DSA_Project/Admin.h
@@ -0,0 +1,13 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+#include "CityGraph.h"
+#include <iostream>
+using namespace std;
+
+#define ADMIN_PASSWORD "admin123"
+
+void adminLogin();
+void adminMenu();
+
+#endif
