14fda6d1b37472f98736b190f413adf12a4f1c6a
Mudassar-56 <mudassarshahbaz6048@gmail.com>
2025-12-16 23:50:29 +0500

    Update visualize.py

--- DSA_Project/visualize.py
+++ DSA_Project/visualize.py
@@ -1,9 +1,7 @@
 import matplotlib.pyplot as plt
 import os
 
-# ============================
-# ALWAYS USE SCRIPT DIRECTORY
-# ============================
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 GRAPH_FILE = os.path.join(BASE_DIR, "graph.txt")
 
